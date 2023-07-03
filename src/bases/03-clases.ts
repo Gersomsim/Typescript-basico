dd1902cc5846e845b62fee238db625a06e0fe76a
Gersomsim <gersomhernandez@me.com>
2023-07-02 21:43:44 -0600

    :technologist: Tipado de peticion

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {Move, PokemonResponse} from "./interfaces/pokeapi-response.interface.ts";
 
 export class Pokemon {
     // public id: number;
@@ -24,8 +25,8 @@ export class Pokemon {
         console.log(`${this.name} ${this.name}`)
     }
 
-    async getMoves(){
-        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+    async getMoves(): Promise<Move[]> {
+        const { data } = await axios.get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon/4');
         return data.moves;
     }
 }
