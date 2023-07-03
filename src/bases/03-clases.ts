67f2c5bc9c259d51517e1891c2c9b7a126acfca9
Gersomsim <gersomhernandez@me.com>
2023-07-02 20:52:59 -0600

    :zap: Getters, Metodos y this

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -2,13 +2,28 @@ export class Pokemon {
     // public id: number;
     // public name: string;
 
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`
+    }
+
     constructor(
         public readonly id: number,
-        public readonly name: string) {
+        public name: string,
+    ) {
         // this.id = id;
         // this.name = name;
+    }
+
+    //metodos
+    scream() {
+        console.log(this.name.toUpperCase())
+    }
+    speak() {
+        console.log(`${this.name} ${this.name}`)
     }
 }
 
 export const charmander = new Pokemon(4, 'charmander');
+charmander.scream();
+charmander.speak();
 
